600b7d2ff407f6d10a5dde2de89df5948b64930f
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2024-09-24 22:23:53 +0100

    chore: first few steps

--- easy1staking/app/page.js
+++ easy1staking/app/page.js
@@ -3,17 +3,20 @@ import HomeHero from "./components/HomeHero";
 import WhyStakeWithUs from "./components/WhyStakeWithUs";
 import WhatWeOffer from "./components/WhatWeOffer";
 import Distribution from "./components/Distribution";
+import WalletProvider from "./components/WalletProvider";
 export default function Home() {
-  
+
   return (
-    <div className="home-bg">
-      <div className="home-hero min-h-[180vh]">
-        <Navbar />
-        <HomeHero />
+    <WalletProvider>
+      <div className="home-bg">
+        <div className="home-hero min-h-[180vh]">
+          <Navbar />
+          <HomeHero />
+        </div>
+        <WhyStakeWithUs />
+        <WhatWeOffer />
+        <Distribution />
       </div>
-      <WhyStakeWithUs />
-      <WhatWeOffer />
-      <Distribution/>
-    </div>
+    </WalletProvider>
   );
 }
