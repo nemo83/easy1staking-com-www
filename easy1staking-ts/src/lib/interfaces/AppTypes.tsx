18454c02a7693d53a56ba63f50dee2a70cf54439
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2024-11-28 22:24:21 +0000

    chore: stats and table

---
+++ easy1staking-ts/src/lib/interfaces/AppTypes.tsx
@@ -0,0 +1,41 @@
+
+export enum EASY1DelegationType {
+    Unregistered = 0,
+    Undelegated = 1,
+    DelegatedOther = 2,
+    Delegated = 3,
+    ConnectWallet = 4,
+    UnsupportedWallet = 5
+}
+
+export enum DistributionType {
+    FixedAmount = 0,
+    MultiplyPerAdaStaked = 1,
+    SplitByStakeAmount = 2,
+    SplitByCount = 3,
+}
+
+export interface Distribution {
+    amount: number;
+    decimales: number;
+    description: string;
+    distribution_model: DistributionType;
+    min_stake_required: number;
+    symbol: string;
+    title: string;
+    token_image_url: string
+}
+
+// WMT Conversion
+
+export interface WmtConversion {
+    tx_hash: string;
+    tx_time: string;
+    amount_wmt_converted: number;
+}
+
+export interface WmtConversionStats {
+    num_conversions_total: number;
+    amount_wmt_converted_total: number
+}
+
