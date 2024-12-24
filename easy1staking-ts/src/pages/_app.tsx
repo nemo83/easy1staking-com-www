eb3f6ff50ed34876cc192bb859434cfe3e999576
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2024-12-24 21:29:39 +0000

    chore: hotjar

--- easy1staking-ts/src/pages/_app.tsx
+++ easy1staking-ts/src/pages/_app.tsx
@@ -4,6 +4,11 @@ import type { AppProps } from "next/app";
 import { MeshProvider } from "@meshsdk/react";
 import Layout from "@/components/Layout";
 
+import Hotjar from '@hotjar/browser';
+const siteId = 3741402;
+const hotjarVersion = 6;
+Hotjar.init(siteId, hotjarVersion);
+
 function App({ Component, pageProps }: AppProps) {
   return (
     <MeshProvider>
