f413a8aaf35fe201ced0546686666e52e54332c6
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2025-04-07 12:58:19 +0100

    chore: more favicons

--- easy1staking-ts/src/pages/_document.tsx
+++ easy1staking-ts/src/pages/_document.tsx
@@ -7,13 +7,13 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
           rel="icon"
-          href="/icon.png"
+          href="/android-chrome-192x192.png"
           type="image/png"
-          sizes="32x32"
+          sizes="192x192"
         />
         <link
           rel="apple-touch-icon"
-          href="/apple-icon.png"
+          href="/apple-touch-icon.png"
           type="image/png"
           sizes="180x180"
         />
