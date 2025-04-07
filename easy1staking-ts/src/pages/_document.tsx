6a3444556d014036cefc173569701fb22ba282fd
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2025-04-07 10:49:07 +0100

    chore: favicon

--- easy1staking-ts/src/pages/_document.tsx
+++ easy1staking-ts/src/pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
